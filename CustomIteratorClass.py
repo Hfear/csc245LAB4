e921a4462ec90b739719e664bdd4b2a09087afd2
Hfear <hannahbfearns@gmail.com>
2025-02-27 18:46:54 -0500

    more

--- CustomIteratorClass.py
+++ CustomIteratorClass.py
@@ -1,1 +1,2 @@
 #Write a custom iterator class Countdown that counts down from a given number to zero.
+
--- itertools.py
+++ itertools.py
@@ -1,1 +1,11 @@
-#Use the itertools.cycle function to cycle through a list of colors ["red", "blue", "green"] indefinitely and print the first 6 values.
+#Use the itertools.cycle function to cycle through a list of colors ["red", "blue","green"]
+# indefinitely and print the first 6 values.
+import itertools
+
+colors = ["red", "blue", "green"]
+
+colorcycle = itertools.cycle(colors)
+
+for color in colorcycle:
+    print(color)
+
