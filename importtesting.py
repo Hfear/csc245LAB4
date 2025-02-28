5cb1279d30407ec9027954a7ec559fece6b730b7
Hfear <hannahbfearns@gmail.com>
2025-02-27 19:06:06 -0500

    testing imprts

---
+++ importtesting.py
@@ -0,0 +1,22 @@
+
+
+# TESTING W3 SCHOOLS CODE
+
+"""from numpy import *
+import numpy
+"""
+"""print(arr)
+print(sum(range(5), -1))"""
+
+"""arr = numpy.array([1, 2, 3, 4, 5])
+print(arr)
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
